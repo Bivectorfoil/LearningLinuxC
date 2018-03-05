f50a8e91367a6b3663ca146edd9bda049921b029
Bivectorfoil <550282600@qq.com>
2018-03-05 10:04:59 +0800

    依照规范，缩进改为8空格

--- Part1/Chapter08/base_array.c
+++ Part1/Chapter08/base_array.c
@@ -5,11 +5,11 @@ int count2[5];
 
 int main(void)
 {
-    // 将一个数组拷贝到另一个
-    int i;
-    for (i = 0; i < 5; i++)
-        count2[i] = count1[i];
-    for (i = 0; i < 5; i++)
-        printf("count1[%d]=[%d]; count2[%d]=[%d]\n", i, count1[i], i, count2[i]);
-    return 0;
+        // 将一个数组拷贝到另一个
+        int i;
+        for (i = 0; i < 5; i++)
+                count2[i] = count1[i];
+        for (i = 0; i < 5; i++)
+                printf("count1[%d]=[%d]; count2[%d]=[%d]\n", i, count1[i], i, count2[i]);
+        return 0;
 }
