7abb38365c2a66ba65d25ccc88e99f36d7e4152b
Bivectorfoil <550282600@qq.com>
2018-03-06 22:41:45 +0800

    章节名：数组； 文件位置：多维数组； 文件类型：练习，剪刀石头布游戏

--- Part1/Chapter08/hand_game.c
+++ Part1/Chapter08/hand_game.c
@@ -4,29 +4,29 @@
 
 int main(void) /* 剪刀石头布游戏 */
 {
-    char gesture[3][10] = { "scissor", "stone", "cloth" };
-    int man, computer, result, ret;
+        char gesture[3][10] = { "scissor", "stone", "cloth" };
+        int man, computer, result, ret;
 
-    srand(time(NULL));
-    while (1) {
-        computer = rand() % 3;
-        printf("\nInput your gesture (0-scissor 1-stone 2-cloth):\n");
-        ret = scanf("%d", &man);
-        if (ret != 1 || man < 0 || man > 2) {
-            printf("Invaild input!\n");
-            return 1;
+        srand(time(NULL));
+        while (1) {
+                computer = rand() % 3;
+                printf("\nInput your gesture (0-scissor 1-stone 2-cloth):\n");
+                ret = scanf("%d", &man);
+                if (ret != 1 || man < 0 || man > 2) {
+                        printf("Invaild input!\n");
+                        return 1;
+                }
+                printf("You: %s\tComputer: %s\n",
+                                gesture[man], gesture[computer]);
+                result = (man - computer + 4) % 3 -1;
+                if (result > 0)
+                        printf("You win!\n");
+                else if (result == 0)
+                        printf("Draw!\n");
+                else
+                        printf("You lose!\n");
         }
-        printf("You: %s\tComputer: %s\n",
-                gesture[man], gesture[computer]);
-        result = (man - computer + 4) % 3 -1;
-        if (result > 0)
-            printf("You win!\n");
-        else if (result == 0)
-            printf("Draw!\n");
-        else
-            printf("You lose!\n");
-    }
-    return 0;
+        return 0;
 }
 
 /*
