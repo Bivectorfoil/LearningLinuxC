3afc20d4cd0f3032ef2c9d17a5225d5d293185c6
Bivectorfoil <550282600@qq.com>
2018-03-07 23:10:47 +0800

    调整switch语句的缩进格式，case语句不缩进

--- Part1/Chapter04/Cond.c
+++ Part1/Chapter04/Cond.c
@@ -13,29 +13,29 @@ int foo(int x)
 void print_day(int day)
 {
         switch (day) {
-                case 1:
-                        printf("Monsday\n");
-                        break;
-                case 2:
-                        printf("Tuesday\n");
-                        break;
-                case 3:
-                        printf("Wednesday\n");
-                        break;
-                case 4:
-                        printf("Thursday\n");
-                        break;
-                case 5:
-                        printf("Friday\n");
-                        break;
-                case 6:
-                        printf("Saturday\n");
-                        break;
-                case 7:
-                        printf("Sunday\n");
-                        break;
-                default:
-                        printf("Illegal day number!\n");
+        case 1:
+                printf("Monsday\n");
+                break;
+        case 2:
+                printf("Tuesday\n");
+                break;
+        case 3:
+                printf("Wednesday\n");
+                break;
+        case 4:
+                printf("Thursday\n");
+                break;
+        case 5:
+                printf("Friday\n");
+                break;
+        case 6:
+                printf("Saturday\n");
+                break;
+        case 7:
+                printf("Sunday\n");
+                break;
+        default:
+                printf("Illegal day number!\n");
         }
 }
 
--- Part1/Chapter06/label.c
+++ Part1/Chapter06/label.c
@@ -5,16 +5,16 @@ int main(void)
         // 章节名：循环语句；文件位置：goto语句和标号；文件类型:习题1答案
         int n = 3;
         switch (n) {
-                case 1:
-                        printf("1\n");
-                        break;
-                case 2:
-                        printf("2\n");
-                        break;
-                        // defau1t:// 编译不会报错，因为编译器将其看成一个标号（label)
-                        //    printf("ohter number\n");
-                default:// 正确的写法
-                        printf("other number\n");
+        case 1:
+                printf("1\n");
+                break;
+        case 2:
+                printf("2\n");
+                break;
+                // defau1t:// 编译不会报错，因为编译器将其看成一个标号（label)
+                //    printf("ohter number\n");
+        default:// 正确的写法
+                printf("other number\n");
         }
         return 0;
 }
