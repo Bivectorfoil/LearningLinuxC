887c8e48cf31d02a482eb3aa58ca9e5cd4a3957b
Bivectorfoil <550282600@qq.com>
2018-03-07 20:51:32 +0800

    章节名：程序的基本概念；文件位置：第一个程序；文件类型：练习

--- Part1/Chapter01/HelloWorld.c
+++ Part1/Chapter01/HelloWorld.c
@@ -3,6 +3,6 @@
 /* main: generate some simple output */
 int main(void)
 {
-    printf("Hello, world.\n"); /* \n 为换行符号 */
-    return 0;
+        printf("Hello, world.\n"); /* \n 为换行符号 */
+        return 0;
 }
