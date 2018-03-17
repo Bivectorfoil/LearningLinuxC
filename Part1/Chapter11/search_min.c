bfdb451f4aeb462f1fe2370c749e15a9de5e268d
Bivectorfoil <550282600@qq.com>
2018-03-17 19:34:31 +0800

    章节名：排序与查找；文件位置：线性查找；文件类型：习题1答案

--- Part1/Chapter11/search_min.c
+++ Part1/Chapter11/search_min.c
@@ -5,9 +5,14 @@ int a[LEN] = { 5, 7, 10, 1, 9, 3, 2, 6 };
 
 int search_min()
 {
-        /* 查找数组中最小值 */
+        /* 查找数组中最小值
+         * 章节名：排序与查找；
+         * 文件位置：线性查找；
+         * 文件类型：习题1答案
+         */
         int i, point;
         i = point = 0;
+
         while (i < LEN) {
                 if (a[i] < a[point]) {
                         point = i;
